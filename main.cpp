ea0652d4c453c3ef96e35b641c221dfae6ae467b
Wolfgang E. Sanyer <wolfgangsanyer@google.com>
2022-10-04 22:42:03 -0400

    Add GetExisting
    and some other stuff

--- main.cpp
+++ main.cpp
@@ -1,30 +1,16 @@
 #include <iostream>
 
 #include "MyProto.pb.h"
+#include "absl/log/check.h"
 #include "absl/status/statusor.h"
 #include "benchmark/benchmark.h"
-
-void ModifyInput(MyProto* my_proto) {
-  my_proto->set_timestamp(1112131415);
-  my_proto->mutable_attributes()->set_resource("something else");
-  my_proto->mutable_attributes()->clear_maybe_blank();
-  my_proto->mutable_attributes()->clear_also_maybe_blank();
-}
-
-absl::StatusOr<MyProto> TransformInput(MyProto my_proto) {
-  my_proto.set_timestamp(1112131415);
-  my_proto.mutable_attributes()->set_resource("something else");
-  my_proto.mutable_attributes()->clear_maybe_blank();
-  my_proto.mutable_attributes()->clear_also_maybe_blank();
-
-  return my_proto;
-}
-
 MyProto make_proto() {
   MyProto my_proto;
   my_proto.set_uid(12345);
   my_proto.set_timestamp(678910);
-  (*my_proto.mutable_metadata())["key"] = "value";
+  for (int i = 0; i < 1000; ++i) {
+    (*my_proto.mutable_metadata())[std::to_string(i)] = "value";
+  }
   Attributes attributes;
   attributes.set_type(TYPEI);
   attributes.set_resource("resource");
@@ -35,11 +21,54 @@ MyProto make_proto() {
   return my_proto;
 }
 
+MyProto GetExisting() {
+  MyProto my_proto;
+  my_proto.set_uid(12345);
+  my_proto.set_timestamp(678910);
+  for (int i = 0; i < 100000; ++i) {
+    (*my_proto.mutable_metadata())[std::to_string(i)] = "value";
+  }
+  Attributes attributes;
+  attributes.set_type(TYPEI);
+  attributes.set_resource("resource");
+  attributes.set_maybe_blank("not blank");
+  attributes.set_also_maybe_blank("also not blank");
+  *my_proto.mutable_attributes() = attributes;
+
+  return my_proto;
+}
+
+absl::Status ModifyInput(MyProto* my_proto) {
+  MyProto local_proto = GetExisting();
+
+  local_proto.set_timestamp(1112131415);
+  local_proto.mutable_attributes()->set_resource("something else");
+  local_proto.mutable_attributes()->clear_maybe_blank();
+  local_proto.mutable_attributes()->clear_also_maybe_blank();
+  local_proto.clear_metadata();
+
+  *my_proto = std::move(local_proto);
+
+  return absl::OkStatus();
+}
+
+absl::StatusOr<MyProto> TransformInput(const MyProto& my_proto) {
+  MyProto local_proto = GetExisting();
+
+  local_proto.set_timestamp(1112131415);
+  local_proto.mutable_attributes()->set_resource("something else");
+  local_proto.mutable_attributes()->clear_maybe_blank();
+  local_proto.mutable_attributes()->clear_also_maybe_blank();
+  local_proto.clear_metadata();
+
+  return local_proto;
+}
+
 void BM_Modify(benchmark::State& state) {
   MyProto my_proto = make_proto();
 
   for (auto s : state) {
-    ModifyInput(&my_proto);
+    CHECK_OK(ModifyInput(&my_proto));
   }
 }
 
@@ -47,16 +76,12 @@ void BM_Transform(benchmark::State& state) {
   MyProto my_proto = make_proto();
 
   for (auto s : state) {
-    if (absl::StatusOr<MyProto> status = TransformInput(my_proto);
-        status.ok()) {
-      my_proto = *status;
-    } else {
-      std::cout << "error: " << status.status();
-    }
+    absl::StatusOr<MyProto> status = TransformInput(my_proto);
+    CHECK_OK(status);
   }
 }
 
 BENCHMARK(BM_Modify);
 BENCHMARK(BM_Transform);
 
-int main() { benchmark::RunSpecifiedBenchmarks(); }
+BENCHMARK_MAIN();
